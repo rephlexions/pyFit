23056b74fc63073c538a3519c002818edc816f0f
Ovidiu Cara <ovidiu92@gmail.com>
2018-11-01 20:51:02 +0100

    Implemented function that plots activity type by duration

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import dateutil.parser
 from matplotlib import dates as mdates
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 class Analyze():
@@ -11,7 +12,7 @@ class Analyze():
 
     def speed(self):
         """
-        Plot average and maximum speed
+        Plot average and maximum speed in km/h
         """
 
         filename = 'Daily Summaries.csv'
@@ -37,6 +38,7 @@ class Analyze():
                         max_speeds.append(max_speed)
                         min_speeds.append(min_speed)
 
+        print(fdates)
         
         fig, ax = plt.subplots(dpi=170)
         fdates = mdates.date2num(fdates)
@@ -48,6 +50,51 @@ class Analyze():
         ax.xaxis.set_major_formatter(date_formatter)
         ax.set_title("Max Speed (km/h)")
         fig.autofmt_xdate()
+        plt.show()
+
+
+    def activity_time(self):
+        """
+        Activity type and duration in minutes
+        """
+
+        filename = 'Daily Summaries.csv'
+        with open(filename) as f:
+            reader = csv.reader(f)
+           
+            cycling_duration, walking_duration, running_duration = 0, 0, 0
+            for row in reader:
+                try:
+                    cycle = int(float(row[14]))/60000
+                    walk = int(float(row[17]))/60000
+                    run = int(float(row[18]))/60000
+                except ValueError:
+                    pass
+                else:
+                    walking_duration = walking_duration + walk
+                    cycling_duration = cycling_duration + cycle
+                    running_duration = running_duration + run
+
+        labels = 'Walking', 'Running', 'Cycling',
+        sizes = [walking_duration, running_duration, cycling_duration]
+        #explode = (0.1, 0.1, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        def make_autopct(values):
+            """
+            Display both the percent value and the original value
+            """
+
+            def my_autopct(pct):
+                total = sum(values)
+                val = int(round(pct*total/100.0))
+                return '{p:.2f}%  ({v:d})'.format(p=pct,v=val)
+            return my_autopct
+
+        fig1, ax1 = plt.subplots(dpi=170)
+        ax1.pie(sizes, labels=labels,colors=['#11cbd7','#c6f1e7','#fa4659'] ,autopct=make_autopct(sizes),
+                shadow=False, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.title('Activity type by percentage and minutes.')
         plt.show()
 
 
@@ -71,13 +118,16 @@ def openFile():
 
 def main():
     print('This program analyzes yout Google Fit data.\n')
-    print('Make sure you have the file Daily Summaries.csv.\n')
+    print('Make sure you have the file \'Daily Summaries.csv\' in the same directory.\n')
     user_input = input('Do you want to start? Y/N \n')
     if user_input == 'Y' or 'y':
-        speed = 'speed'
-        if speed == 'speed':
+        user_input = input('Type?\n')
+        if user_input == 'speed':
             graph_speed = Analyze()
             graph_speed.speed()
+        elif user_input == 'time':
+            bar_time = Analyze()
+            bar_time.activity_time()
     else:
         exit
 
